7eeede8ade90e40e7eb21819bd2eac6c630e9692
jrr07 <strongjoy07@gmail.com>
2022-01-22 12:22:15 +0900

    add

--- 2022-01-15/main.cpp
+++ 2022-01-15/main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include "Circle.h"
 
-//#include "qlist.h"
+#include "qlist.h"
 
 using namespace std;
 
@@ -12,19 +12,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    /*int *x = nullptr;
-    x = new int;
-    *x = 10;
 
-    cout << x << endl;
-    int y;
-    y = 5;
-    *x = y + *x;
-    cout << y << endl;
-    cout << *x;
-    delete x;
-    x = nullptr;*/
-
+    //2번 숙제 /////////////////////////////////////////////////
     Line * line = nullptr;
     line = new Line(0, 0, 5, 5, true);
 
@@ -45,51 +34,48 @@ int main(int argc, char *argv[])
     cout << "circle radius: " << circle->getRadius() << endl;
     cout << "circle color: " << circle->getColor() << endl;
 
-   /* line->print();*/
+
     delete line;
     line = nullptr;
     delete circle;
     circle = nullptr;
-    return 0;
+    // ///////////////////////////////////////////////////////////////
 
+
+
+    // 3번 숙제 ///////////////////////////////////////////////////////
     QList<Figure*> list;
 
-  /*  Line line(0,0,1,1,true);
+    Line * line2 = nullptr;
+    line2 = new Line (0,0,5,5,true);
 
-    cout << "line startX: " << line.getStartX() << endl;
-    cout << "line startY: " << line.getStartY() << endl;
-    cout << "line endX: " << line.getEndX() << endl;
-    cout << "line endY: " << line.getEndY() << endl;
+    Circle * circle2 = nullptr;
+    circle2 = new Circle (0,0,5,true);
 
-    cout << "color " << line.getColor() << endl;
+    list.append(line2);
 
+    list.append(circle2);
 
-    Circle circle(0,0,1,true);
-    cout << "circle x: " << circle.getCenterX() << endl;
-    cout << "circle y: " << circle.getCenterY() << endl;
-    cout << "circle radius: " << circle.getRadius() << endl;
-    cout << "circle color: " << circle.getColor() << endl;
+    for(int i = 0; i < list.count(); i++){
+        delete list.at(i);
+    }
 
+    list.clear();
 
-
-    Line *line2 = new Line(3,3,5,5, false);
-    cout << "line2 startX: " << line2->getStartX() << endl;
-    cout << "line2 startY: " << line2->getStartY() << endl;
-    cout << "line2 endX: " << line2->getEndX() << endl;
-    cout << "line2 endY: " << line2->getEndY() << endl;
-
-    cout << "color " << line2->getColor() << endl;
-
-    Circle *circle2 = new Circle(2,2,4,true);
-    cout << "circle2 x: " << circle2->getCenterX() << endl;
-    cout << "circle2 y: " << circle2->getCenterY() << endl;
-    cout << "circle2 radius: " << circle2->getRadius() << endl;
-    cout << "circle2 color: " << circle2->getColor() << endl;
-
-    delete line2;
     line2 = nullptr;
-    delete circle2;
-    circle2 = nullptr;*/
+    circle2 = nullptr;
+
+
+
+
+
+
+
+
+
+
+
+    // //////////////////////////////////////////////////////////////////
 
     return 0;
 
