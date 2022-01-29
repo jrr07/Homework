ab19c27ac5944d1d27571f822bd7dbae66db4ba8
jrr07 <strongjoy07@gmail.com>
2022-01-29 12:53:40 +0900

    study

--- 2022-01-22/Circle.cpp
+++ 2022-01-22/Circle.cpp
@@ -36,8 +36,19 @@ int Circle::getRadius(){
 Circle::~Circle(){
 
 }
-
-
 void Circle::whoAreYou(){
     std::cout << "i am circle" << std::endl;
+}
+void Circle::setData(int centerX, int centerY, int radius){
+    this->centerX = centerX;
+    this->centerY = centerY;
+    this->radius = radius;
+
+}
+void Circle::setData(int centerX){
+    this->centerX = centerX;
+}
+void Circle::setData(int centerX, int centerY){
+    this->centerX = centerX;
+    this->centerY = centerY;
 }
