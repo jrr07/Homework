861ff14317fbbd2d93c6e40fbab2aeee0fba8c0e
jrr07 <97771569+jrr07@users.noreply.github.com>
2022-01-29 11:43:47 +0900

    Homework (#2)
    * 2022-01-22
    
    * add
    
    * study

---
+++ 2022-01-22/Figure.h
@@ -0,0 +1,17 @@
+#ifndef FIGURE_H
+#define FIGURE_H
+
+
+class Figure{
+protected:
+    bool color;
+public:
+    Figure();
+    Figure(bool color);
+    void setColor(bool color);
+    bool getColor();
+    virtual ~Figure() =0; //추상클래스 선언
+};
+
+
+#endif // FIGURE_H
